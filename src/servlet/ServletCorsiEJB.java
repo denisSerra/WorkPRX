6de27a8bbc0cc9ce73130543092036e2dd7fe9da
Terminale3 <Terminale3@CENTAURI4>
2018-11-09 17:42:44 +0100

    new commit

--- src/servlet/ServletCorsiEJB.java
+++ src/servlet/ServletCorsiEJB.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ejb.CorsiEJBRemote;
+import function.FunctionCorsi;
 
 /**
  * Servlet implementation class ServletEJB
@@ -18,39 +20,53 @@ import ejb.CorsiEJBRemote;
 @WebServlet("/ServletCorsiEJB")
 public class ServletCorsiEJB extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	 @Resource(mappedName="java:jboss/exported/Corsi/CorsiEJB!ejb.CorsiEJBRemote")
-	    private CorsiEJBRemote corsiEJBRemote;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletCorsiEJB() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-   
+	@Resource(mappedName = "java:jboss/exported/Corsi/CorsiEJB!ejb.CorsiEJBRemote")
+	private CorsiEJBRemote corsiEJBRemote;
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/InserimentoCorso.jsp");
-		dispatcher.forward(request, response);
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public ServletCorsiEJB() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		try {
-//			corsiEJBRemote.find();
-//			corsiEJBRemote.inserisciCorso(2, "JavaEE");
-//			corsiEJBRemote.modificaNomeCorsoById(1,"gonfia palloncini");
-//			corsiEJBRemote.deleteCorso(1);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Home.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String idCorso = request.getParameter("id");
+		String nomeCorso = request.getParameter("nomecorso");
+		boolean inserito = false;
+		if (idCorso != null && nomeCorso != null) {
+			try {
+				inserito = FunctionCorsi.inserisciCorso(idCorso, nomeCorso);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			if (inserito) {
+				RequestDispatcher req = request.getRequestDispatcher("/WEB-INF/Inserimento.jsp");
+				req.forward(request, response);
+			} else {
+				RequestDispatcher req = request.getRequestDispatcher("/WEB-INF/InserisciImp.jsp");
+				req.forward(request, response);
+			}
 		}
 	}
 }
